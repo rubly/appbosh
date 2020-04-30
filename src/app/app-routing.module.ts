db7c314f539258d927b380116e2161d152fa89e7
Bryan Morgan <Bryan.Morgan@optimumcredit.co.uk>
2020-04-30 14:25:34 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IntervalTimerComponent } from './interval-timer/interval-timer.component';
+import { AppComponent } from './app.component';
+import { DefaultComponent } from './default/default.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent,
+  },
+  {
+    path: 'intervals',
+    component: IntervalTimerComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntervalTimerComponent } from './interval-timer/interval-timer.component';
+import { DefaultComponent } from './default/default.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntervalTimerComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
